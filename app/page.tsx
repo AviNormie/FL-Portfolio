fd71afd3c907a0e0cd7585e89e509c2e5b00915e
vishuRizz <vishurizz0@gmail.com>
2025-04-18 23:52:01 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import My3DScene from "@/components/My3DScene";
-import Image from "next/image";
+import ContactUsPage from "@/components/Contact";
+import ExperienceAndTools from "@/components/Experience";
+import Hero from "@/components/Hero";
+import RecentProjects from "@/components/Projects";
 
 export default function Home() {
   return (
    <>
-   <div className="h-screen w-full bg-black">
-    hiiii
+   <div className="h-screen w-full bg-[#141312]">
+      <Hero/>
+      <RecentProjects/>
+      <ExperienceAndTools/>
+      <ContactUsPage/>
    </div>
   
    </>
